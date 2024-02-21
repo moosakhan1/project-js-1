e7851400b03d4f4328b161644d423eef153c7e04
Moosakhan <144359037+moosakhan1@users.noreply.github.com>
2024-02-21 22:40:22 +0500

    assigment 1

---
+++ app.js
@@ -0,0 +1,71 @@
+// // chapter 1
+
+// // question 1
+
+// alert("welcome");
+
+// // question 2
+
+// alert("Error! enter a valid password.")
+
+// // question 3
+
+// alert("Welcome to JS land... \n Happy Coding!")
+
+// // question 4
+
+// alert("Welcome to JS land... ")
+
+// alert("Happy Coding!")
+
+// // question 5
+
+// alert("Holle...i can run js through my web browser's console")
+
+// // chapter 2
+
+// // question 1
+
+// var userName;
+
+// // question 2
+
+// var myName;
+
+// myName = "Moosa khan";
+
+// // question 3
+
+// var message;
+
+// message = "Hello World"
+
+// alert(message)
+
+// question 4
+
+var name = "Moosa khan" 
+var age = "15 year old"
+var mobile = "devlopment"
+
+alert(name)
+alert(age)
+alert(mobile)
+
+// question 5
+
+var name1 = "PIZZA \n PIZZ \n PIZ \n P"
+alert(name1)
+
+// question 6
+
+var email;
+email = " moosakhan033233@gmail.com"
+alert("my email address is" + email)
+
+// question 7
+var book = "A smarter way to \n learn JAVASCRIPT"
+alert(book)
+
+// question 8
+document.write("<center><p>Yah! I can write HTML content through JAVASCRIPT</p></center>")
